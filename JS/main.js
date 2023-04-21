46198dccac9a41e555f15b8ce23f168f970bdac7
miss <ddc.akari@gmail.com>
2023-04-22 04:25:56 +0900

    footer 작업

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 import ipads from "../assets/data/ipads.js";
+import navigations from "../assets/data/navigations.js";
 
 /* 장바구니 드롭다운 메뉴 */
 const basketStarterEl = document.querySelector(".basket-starter");
@@ -119,3 +120,34 @@ ipads.forEach((ipad) => {
   `;
   itemsEl.append(itemEl);
 });
+
+// 푸터 내비게이션 맵 랜더링!
+const navigationsEl = document.querySelector("footer .navigations");
+navigations.forEach((nav) => {
+  const mapEl = document.createElement("div");
+  mapEl.classList.add("map");
+
+  let mapList = "";
+  nav.maps.forEach((map) => {
+    mapList += /* html */ `
+    <li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`;
+  });
+
+  mapEl.innerHTML = /* html */ `
+    <h3>
+      <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `;
+
+  navigationsEl.append(mapEl);
+});
+
+// 올해 연도를 적용!
+const thisYearEl = document.querySelector(".this-year");
+thisYearEl.textContent = new Date().getFullYear();
